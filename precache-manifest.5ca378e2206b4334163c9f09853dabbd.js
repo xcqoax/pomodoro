688d86ef281490e6d6070ba3918cac81dcded965
Gonzalo <palmer_85@hotmail.com>
2020-07-15 20:52:17 -0500

    Updates

---
+++ precache-manifest.5ca378e2206b4334163c9f09853dabbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778f943a429d10a4c4cec4085fe8a08d",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "6b059002e4a739218ee6",
+    "url": "/pomodoro/static/css/main.e88ac515.chunk.css"
+  },
+  {
+    "revision": "a06900e632f2217ad43d",
+    "url": "/pomodoro/static/js/2.e59ed367.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.e59ed367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b059002e4a739218ee6",
+    "url": "/pomodoro/static/js/main.77e033df.chunk.js"
+  },
+  {
+    "revision": "ee3590aa5d45d2dacf12",
+    "url": "/pomodoro/static/js/runtime-main.f3456f13.js"
+  }
+]);
